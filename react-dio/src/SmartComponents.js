60a8c02424804e25694b3381a0b44f867ee8fb44
vinicius <vfonseca.eng@gmail.com>
2022-06-23 07:43:55 -0300

    Aula - Smart Components

---
+++ react-dio/src/SmartComponents.js
@@ -0,0 +1,9 @@
+// SMART COMPONENTS:
+
+// - Preocupam-se como as coisas vão funcionar 
+// - Podem conter Smart e Dumb Components
+// - Providenciam dados e padrões de apresentação ou outros containers 
+// - N maioria dos casos possuem estado e podem chamar outros fluxos do sistema.
+
+// EXEMPLOS: UserGallery, UserPage, FilterBook, FollowersSidebar, ListCards
+
